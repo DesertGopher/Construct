025f9b0709d47781af10257d48f75c0705d61579
desertgopher <quasar360@mail.ru>
2023-04-09 02:34:35 +0300

    Added image to news reply + list of categories

--- telebot/bot_one.py
+++ telebot/bot_one.py
@@ -20,17 +20,20 @@ token = os.getenv('bot_token')
 bot = Bot(token=token)
 dp = Dispatcher(bot)
 
+_NP_PHOTO_PATH = os.path.abspath(CONF_DIR / 'construct' / 'media')
+
 
 @dp.message_handler(commands="start")
 async def cmd_start(message: types.Message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     buttons = ["Категории товаров", "Что новенького?"]
     keyboard.add(*buttons)
-    await message.answer("Как подавать котлеты?", reply_markup=keyboard)
+    await message.answer("Приветствуем в лучшем в мире боте для ВКР по вебу. Чем могу быть полезен?",
+                         reply_markup=keyboard)
 
 
 @dp.message_handler(Text(equals="Что новенького?"))
-async def with_puree(message: types.Message):
+async def whats_new(message: types.Message):
     result = requests.get('http://127.0.0.1:8005/news/get_last')
     reply = str(
         "Из последних новостей:" + '\n' +
@@ -38,13 +41,23 @@ async def with_puree(message: types.Message):
         '<i>' + str(result.json()['news']) + '</i>' + '\n' +
         '<u>' + 'от ' + str(result.json()['pub_date'][:10]) + '</u>'
     )
-    await message.reply(str(reply), parse_mode="html")
+    pic = open((str(_NP_PHOTO_PATH) + '/' + str(result.json()['picture'])).replace('\\', '/'), 'rb')
+    await bot.send_photo(message.from_user.id, photo=pic, caption=str(reply),
+                         reply_to_message_id=message.message_id, parse_mode='html')
 
 
-@dp.message_handler(lambda message: message.text == "Категории товаров")
-async def without_puree(message: types.Message):
-    await message.reply("Так невкусно!")
-
+@dp.message_handler(Text(equals="Категории товаров"))
+async def whats_new(message: types.Message):
+    result = requests.get('http://127.0.0.1:8005/categories/get-list')
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    buttons = []
+    reply = str('Вот что мы имеем...' + '\n')
+    for i in result.json():
+        buttons.append(i['name'])
+        reply += ' - <i>' + str(i['name']) + '</i>\n'
+    keyboard.add(*buttons)
+    reply += "Какая категория Вас интересует?"
+    await message.answer(reply, reply_markup=keyboard, parse_mode='html')
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
